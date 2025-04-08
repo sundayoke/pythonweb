410cfe1601e9a37d107452638f31019681493fa8
Sunday Oke <sunday.oke@gmail.com>
2025-04-08 22:01:53 +0200

    dhdgh

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/sunday/Documents/pythonweb/env
+set -gx VIRTUAL_ENV /home/sunday/Documents/pythonweb/pythonweb/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
